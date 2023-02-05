a75061d782c660406669a266eda99d68ae18472e
JuanGabrielArenasMeneses <122894273+JuanGabrielArenasMeneses@users.noreply.github.com>
2023-02-05 09:54:45 -0500

    Correccion del metodo borrar el ultimo elemento

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -33,10 +33,10 @@ export class CalculatorComponent implements OnInit {
     console.log (element.value);
     if (!(this.flag)) {
       this.number1=this.number1+element.value;
-      console.log (this.number1);
+      console.log ('number 1 '+this.number1);
     } else {
       this.number2=this.number2+element.value;
-      console.log (this.number2);
+      console.log ('number 2 '+this.number2);
     }
     
     
@@ -44,35 +44,32 @@ export class CalculatorComponent implements OnInit {
   negate(): void {
     if (!(this.flag)) {
       this.number1= String(parseInt(this.number1)*(-1));
-      console.log (this.number1);
+      console.log ('number 1 '+this.number1);
     } else {
       this.number2= String(parseInt(this.number2)*(-1));
-      console.log (this.number2);
+      console.log ('number 2 '+this.number2);
     }
   }
   clear(): void {
     
       this.number1= '';
-      console.log (this.number1);
+      console.log ('number 1 '+this.number1);
     
       this.number2= '';
-      console.log (this.number2);
+      console.log ('number 2 '+this.number2);
     
   }
   delete(): void {
     if (!(this.flag)) {
       let num: string[]=this.number1.split('');
+      this.number1=String((num.slice(0,(num.length)-1))).replace(/,/g,'');
       console.log (num);
-      num.pop;
-      this.number1=(num).toString();
-      console.log (num);
-      console.log (this.number2);
+      console.log ('number 1 '+this.number1);
     } else {
       let num: string[]=this.number2.split('');
-      num.pop;
-      this.number1=(num).toString();
+      this.number2=String((num.slice(0,(num.length)-1))).replace(/,/g,'');
       console.log (num);
-      console.log (this.number2);
+      console.log ('number 2 '+this.number2);
     }
   } 
   percent(): void {
